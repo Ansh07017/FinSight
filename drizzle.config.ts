ce13b61b4bf5f55b6ffa0e281284730fa4b48f78
Ansh Pratap Singh <pratapsingh07017@gmail.com>
2026-01-17 13:34:10 +0530

    updated config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
 dotenv.config();
+
 if (!process.env.PG_CONNECTION_STRING) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
+  throw new Error("PG_CONNECTION_STRING is missing in .env");
 }
 
 export default defineConfig({
@@ -12,5 +13,8 @@ export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.PG_CONNECTION_STRING,
+    ssl: {
+      rejectUnauthorized: false
+    }
   },
 });
